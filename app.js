17ab0eeb083e0710e7a140ee0f1fd124da0d5046
jay-juns <necrozerg2@naver.com>
2021-01-26 15:55:11 +0900

    3번째 시간

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 "use strict";
-var logo = 10;
+var bio = '나는 문자열이다';
+var score = 233;
+var go = true;
+var year = 2021;
+var score1 = {
+    bio: '바꿔보자 정보를',
+    score: 12,
+    year: 2021,
+    go: false
+};
+function getInfomation(score) {
+    return {
+        bio: '바꿔보자 정보를',
+        score: 12,
+        go: false
+    };
+}
+function saveScore(saveScore) {
+}
+saveScore(score1);
--- app.ts
+++ app.ts
@@ -1,1 +1,34 @@
-let logo = 10;
+let bio:string = '나는 문자열이다';
+let score:number = 233;
+let go: boolean = true;
+let year: number = 2021;
+
+let score1 = {
+  bio:'바꿔보자 정보를',
+  score:12,
+  year: 2021,
+  go: false
+};
+
+interface Point {
+  readonly bio:string;
+  score:number;
+  year?: number;
+  go: boolean;
+  addComment?: (comment:string) => string;
+}
+
+function getInfomation(score: number): Point 
+{
+  return {
+    bio:'바꿔보자 정보를',
+    score:12,
+    go: false
+  }  
+}
+
+function saveScore(saveScore: Point): void {
+ 
+}
+
+saveScore(score1);
